58734f988446224d93c990ab7f8c7d637ec2935c
Simon Charest <simoncharest@gmail.com>
2024-02-14 13:05:29 -0500

    Fixing the CRUD functionalities in the UI

--- apy/main.py
+++ apy/main.py
@@ -1,10 +1,19 @@
+import os
 from fastapi import FastAPI, Response
+from fastapi.middleware.cors import CORSMiddleware
 from json import loads
 from pathlib import Path
 from sqlite3 import connect, Connection
 from uvicorn import run
 
 app: FastAPI = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 connection: Connection = connect(Path(__file__).parent.joinpath("data/apy.db"))
 
 
@@ -23,13 +32,20 @@ async def root() -> str:
         content += f"""
                 <tr>
                     <td>{row[0]}</td>
-                    <form action='http://127.0.0.1:8000/users/update/{row[0]}' method='POST'>
-                        <td><input name='first_name' value='{row[1]}'></td>
-                        <td><input name='last_name' value='{row[2]}'></td>
-                        <td><input name='email' value='{row[3]}'></td>
-                        <td class='text-align-center'><input name='update' type='submit' value='☑' onclick='updateUser({row[0]});'></td>
-                    </form>
-                    <td class='text-align-center'><input name='delete' type='button' value='☒' onclick='deleteUser({row[0]})'></td>
+                    <td><input id='firstName[{row[0]}]' value='{row[1]}'></td>
+                    <td><input id='lastName[{row[0]}]' value='{row[2]}'></td>
+                    <td><input id='email[{row[0]}]' value='{row[3]}'></td>
+                    <td class='text-align-center'><input name='update' type='button' value='☑' onclick='updateUser({row[0]});'></td>
+                    <td class='text-align-center'><input name='delete' type='button' value='☒' onclick='deleteUser({row[0]});'></td>
+                </tr>"""
+        
+    content += f"""
+                <tr>
+                    <td></td>
+                    <td><input id='firstName' value=''></td>
+                    <td><input id='lastName' value=''></td>
+                    <td><input id='email' value=''></td>
+                    <td class='text-align-center' colspan="2"><input name='insert' type='button' value='⨁' onclick='insertUser();'></td>
                 </tr>"""
 
     return Response(document.replace("%CONTENT%", content), media_type="text/html")
@@ -119,7 +135,6 @@ async def insert(users: list[dict]) -> dict:
     return {"message": "User created."}
 
 
-@app.post("/users/update/{id}")
 @app.put("/users/update/{id}")
 async def update(id: str, user: dict) -> dict:
     sql: str = Path(__file__).parent.joinpath("data/users/update.sql").read_text()
@@ -129,6 +144,15 @@ async def update(id: str, user: dict) -> dict:
     connection.commit()
 
     return {"message": "User updated."}
+
+
+@app.post("/users/test/{id}")
+@app.put("/users/test/{id}")
+async def test(id: str, user: dict) -> dict:
+    print(id)
+    print(user)
+
+    return {"message": "OK"}
 
 
 @app.delete("/users/delete/{id}")

6911af180ade5be321dfe6d2f4d1cf45b81edf2e
Simon Charest <simoncharest@gmail.com>
2024-02-16 17:14:02 -0500

    Adding a test for the mortgage function

--- apy/tests/test_mortgage.py
+++ apy/tests/test_mortgage.py
@@ -4,7 +4,7 @@ from pytest import mark
 
 class TestMortgage:
     @mark.asyncio
-    async def test_mortgage_add(self):
+    async def test_mortgage_median(self):
         # Arrange
         request: dict = {
             "loan": 506500.00,
@@ -26,6 +26,43 @@ class TestMortgage:
                 "total": {
                     "interest": 412142.0,
                     "payment": 918642.0
+                },
+                "rate": {
+                    "interest": 0.4486,
+                    "payment": 1.81
+                }
+            }
+        }
+
+        # Act
+        actual: dict = await mortgage(request)
+
+        # Assert
+        assert expected == actual
+
+    @mark.asyncio
+    async def test_mortgage_2539_rue_l_o_david_montreal(self):
+        # Arrange
+        request: dict = {
+            "loan": 1786000.00,
+            "rate": 0.0534,
+            "duration": 25
+        }
+        expected: dict = {
+            "request": {
+                "loan": 1786000.0,
+                "rate": 0.0534,
+                "duration": 25
+            },
+            "response": {
+                "monthly": {
+                    "capital": 5953.8,
+                    "interest": 4843.81,
+                    "payment": 10797.61
+                },
+                "total": {
+                    "interest": 1453283.0,
+                    "payment": 3239283.0
                 },
                 "rate": {
                     "interest": 0.4486,
